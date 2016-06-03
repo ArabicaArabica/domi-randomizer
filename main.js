bb6d3087868a6184cad6a19dd230c713f27fbd52
ArabicaArabica <nnfsb049@ybb.ne.jp>
2016-06-04 05:44:29 +0900

    これはmonadなのか

--- main.js
+++ main.js
@@ -24,28 +24,59 @@
     );
   });
 
-  $("button").click(function(event){
-    try{
-      var candidates = GLOBAL_EXTENDS.filter(function(elem, ind, arr){
-        return $("#ex" + ind).prop("checked")
-      }).map(function(candidate, i, arr){
-        return cards.filter(function(card){
-          return card.extend === candidate;
-        });
-      }).reduce(function(p, c){
-        return p.concat(c);
-      });
-      var results = _.range(10).map(function(i){
-        return candidates.splice(
-          Math.floor(Math.random() * candidates.length), 1
-        )[0];
-      });
-    }catch(e){
-      return ;
+//Monad
+  var Maybe = {
+    Some : function(arr){
+      this.arr = arr;
+      this.length = arr.length;
+      this.bind = function(f){
+        return f(this.arr);
+      };
+    },
+    Nothing : function(){
+      this.bind = function(){ return this; };
+      this.length = 0;
+    },
+    return : function(arr){ return new Maybe.Some(arr); }
+  };
+  function first(arr){
+    return arr.length === 0 ? new Maybe.Nothing() : new Maybe.Some(arr);
+  }
+  function reduce(M, f){
+    return M.bind(function(arr){ return new Maybe.Some(arr.reduce(f)); });
+  }
+  function splice(M, a, b){
+    return M.bind(function(arr){ return new Maybe.Some(arr.splice(a, b)); });
+  }
+  function prepare(M){
+    if(M.length != 0){
+      $("#result").show();
+      $("#result > tbody > *").remove();
     }
-    $("#result").show();
-    $("#result > tbody > *").remove();
-    results.forEach(function(elem){
+    return M;
+  }
+
+  $("button").click(function(event){
+    var candidates = GLOBAL_EXTENDS.filter(function(elem, ind, arr){
+      return $("#ex" + ind).prop("checked")
+    }).map(function(candidate, i, arr){
+      return cards.filter(function(card){
+        return card.extend === candidate;
+      });
+    });
+    var cand = prepare(
+      reduce(
+        first(candidates)
+      , function(p, c){ return p.concat(c); }
+      )
+    );
+    _.range(10).map(function(i){
+      return splice(
+        cand
+      , Math.floor(Math.random() * cand.bind(function(arr){ return arr.length; }))
+      , 1
+      ).bind(function(arr){ return arr[0]; });
+    }).forEach(function(elem){
       $("#result > tbody").append($("<tr>")
         .append($("<td>").text(elem.name))
         .append($("<td>").text(elem.extend))
